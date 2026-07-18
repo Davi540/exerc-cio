22b68cfb1ebbde38fdbe6955dd8faeb97f49fef5
davi540 <davilemesdaurte@gmail.com>
2026-07-18 18:29:41 -0300

    v1.1

---
+++ js/exercicio4-5.js
@@ -0,0 +1,15 @@
+function mostrarDescricao(){
+
+    let descricao = document.getElementById("descricao");
+
+    if(descricao.style.display === "none"){
+
+        descricao.style.display = "block";
+
+    }else{
+
+        descricao.style.display = "none";
+
+    }
+
+}
